f1301764cb5813736b3152eb663d7c9619879818
bernatskak <106559001+bernatskak@users.noreply.github.com>
2022-11-12 02:16:12 +0200

    finish 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,50 +1,58 @@
 import { galleryItems } from "./gallery-items.js";
-// Change code below this line
-const galleryUl = document.querySelector('.gallery');
-const cardsGallery = createGalleryItems(items);
 
-galleryUl.insertAdjacentHTML('beforeend', cardsGallery);
+const galleryUl = document.querySelector(".gallery");
+const cardsGallery = createGalleryItems("item");
 
-function createGalleryItems(items) {
-    return galleryItems
-        .map(({ preview, original, description }) => {
-        return '       
-            <div class ="gallery__item" >
-                <a class="gallery__link" href="large-image.jpg">
+galleryUl.insertAdjacentHTML("beforeend", cardsGallery);
+galleryUl.addEventListener("click", onGalleryUlClick);
+
+function createGalleryItems(item) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+            <div class ="gallery__item">
+                <a class="gallery__link" href=${original}>
                     <img
                         class="gallery__image"
                         src="${preview}"
                         data-source="${original}"
                         alt="${description}"
                     />
-                </a>  
-         </div >';
+                </a>
+         </div >`;
     })
-.join(''); 
+    .join("");
+}
+function onGalleryUlClick(eve) {
+  eve.preventDefault();
+  if (eve.target.nodeName !== "IMG") return;
 }
 
+// console.log(eve.target.dataset.source);
 
+// const imageEl = document.querySelector(".gallery__item");
+// imageEl.src === eve.target.dataset.source;
+const instance = basicLightbox.create(
+  ` <div class="modal">
+         <img
+       src= "${eve.target.dataset.source}"
+    />
+    </div>
+     `,
+  {
+    onShow: (instance) => {
+      window.addEventListener("keydown", onEscPress);
+      instance.element().querySelector("img").onclick = instance.close;
+    },
+    onClose: (instance) => {
+      window.removeEventListener("keydown", onEscPress);
+    },
+  }
+);
+function onEscPress(eve) {
+  if (eve.code === "Escape") {
+    instance.close();
+  }
+}
 
-
-
-
-// const gallery = document.querySelector("gallery");
-
-// gallery.addEventListener("click", onclick);
-
-// function onClick(event) {
-//   if (event.target.nodeName !== "IMG") {
-//     return;
-//   }
-//   console.log(event.target.textContent);
-// }
-
-// const instance = basicLightbox.create(`
-//     <div class="modal">
-//         <p>
-//             Your first lightbox with just a few lines of code.
-//             Yes, it's really that simple.
-//         </p>
-//     </div>
-// `);
-
+instance.show();
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,16 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryEl = document.querySelector("ul.gallery");
 
+function makeGallery(arr) {
+  const galleryItemsMarkup = arr
+    .map(({ preview, original, description }) => {
+      return `<li><a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}" title="${description}" /></a></li>`;
+    })
+    .join("");
+  return galleryEl.insertAdjacentHTML("afterbegin", galleryItemsMarkup);
+}
+
+makeGallery(galleryItems);
+const gallery = new SimpleLightbox("ul.gallery a");
 console.log(galleryItems);
